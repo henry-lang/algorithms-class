44c708ac2753918ad44c65cf253dafd654a23132
henry-lang <henry.langmack@gmail.com>
2023-09-17 21:23:37 -0400

    first commit

---
+++ src/ThreeSum.java
@@ -0,0 +1,48 @@
+package src;
+
+// Credit: Computer Science - An Interdisciplinary Approach
+// I did not write this code, but I have analyzed it and that is why this is in the repository.
+
+// Some issues with this algorithm:
+// Both the printAll and count methods run in O(n^3) time, which does not scale well with large values of N.
+// Other, than that, the code works as expected if n >= 3.
+
+public class ThreeSum {
+    // print distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
+    public static void printAll(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+                    }
+                }
+            }
+        }
+    }
+
+    // return number of distinct triples (i, j, k) such that a[i] + a[j] + a[k] = 0
+    public static int count(int[] a) {
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] a = StdIn.readAllInts();
+        Stopwatch timer = new Stopwatch();
+        int count = count(a);
+        StdOut.println("elapsed time = " + timer.elapsedTime());
+        StdOut.println(count);
+    }
+}
