577a445c5ce7b8c6e6c5cb22982d29ad3b554afd
henry-lang <henry.langmack@gmail.com>
2023-12-07 21:07:39 -0500

    improve code

--- RadixSort.java
+++ RadixSort.java
@@ -11,38 +11,35 @@ class RadixSort {
         int[] newArr = new int[arr.length];
 
         int maxNum = 0;
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             maxNum = Math.max(maxNum, arr[i]);
         }
-    
+
         int maxDigits = maxNum == 0 ? 1 : (int) (Math.log10(maxNum) + 1);
 
-        for(int d = 0; d < maxDigits; d++) {
+        for (int d = 0; d < maxDigits; d++) {
             int place = (int) Math.pow(10, d);
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 digit[(arr[i] / place) % 10]++;
             }
 
-            for(int i = 1; i < 10; i++) {
+            for (int i = 1; i < 10; i++) {
                 digit[i] = digit[i] + digit[i - 1];
             }
 
-            for(int i = arr.length - 1; i >= 0; i--) {
+            for (int i = arr.length - 1; i >= 0; i--) {
                 int thisDigit = (arr[i] / place) % 10;
-                // System.out.println(arr[i] + " " + thisDigit);
                 newArr[digit[thisDigit] - 1] = arr[i];
                 digit[thisDigit] -= 1;
             }
 
-            for(int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {
                 digit[i] = 0;
             }
 
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 arr[i] = newArr[i];
             }
-            
-            System.out.println(Arrays.toString(newArr));
         }
 
         return arr;
@@ -50,13 +47,14 @@ class RadixSort {
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        
+
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = (int)(Math.random() * 10000);
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int) (Math.random() * 1000);
         }
 
-        System.out.println(Arrays.toString(arr));
-        radixSort(arr);
+        System.out.println("Before: " + Arrays.toString(arr));
+        int[] newArr = radixSort(arr);
+        System.out.println("After:  " + Arrays.toString(newArr));
     }
 }
